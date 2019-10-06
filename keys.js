9cef8b773442fd03fa382685894ba86854fb3981
cecileanne <cecileannesison@gmail.com>
2019-10-05 22:19:48 -0400

    added axios calls (still broken) and log.txt

--- keys.js
+++ keys.js
@@ -1,6 +1,14 @@
-console.log("this is loaded");
+// console.log("this is loaded");
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
+};
+
+exports.seatgeek = {
+  clientKey: process.env.SEATGEEK_CLIENT_ID
+};
+
+exports.omdb = {
+  APIkey: process.env.OMDB_APIKEY
 };
--- liribot.js
+++ liribot.js
@@ -1,16 +1,25 @@
 require("dotenv").config();
 const inquirer = require("inquirer");
+const seatgeek = require("./keys.js");
 const Spotify = require("node-spotify-api");
 const axios = require("axios");
 const moment = require("moment");
 const fs = require("fs");
 const keys = require("./keys.js");
 
+// // link seatgeek key to this file - NEED TO FIGURE OUT FOR LATER
+// seatgeek.connect({
+//   clientKey: process.env.SEATGEEK_CLIENT_ID
+// });
+
 // console.log(keys.spotify) to see if it's linking correctly;
 const spotify = new Spotify(keys.spotify);
 
 // wrap everything in a function
 function cli() {
+  // Create a Divider for outputs
+  const divider =
+    "\n------------------------------------------------------------\n\n";
   // Make it so liri.js can take in one of the following commands:
   inquirer
     .prompt([
@@ -28,16 +37,46 @@ function cli() {
     ])
     .then(function(searchType) {
       if (searchType.selection === "Find upcoming shows for a certain band") {
-        // function(concertThis) {
-        console.log(
-          "HERE YOU WILL PUT THE PROMPT FOR BAND SEARCHES followed by the search"
-        );
-        // concert-this
-        // https://api.seatgeek.com/2/events?performers.slug=<PERFORMER-STRING>&client_id=<CLIENT_ID>"
-        // Name of the venue
-        // Venue location
-        // Date of the Event (use moment to format this as "MM/DD/YYYY")
-        // } // closes concertThis function
+        // console.log(
+        //   "HERE YOU WILL PUT THE PROMPT FOR BAND SEARCHES followed by the search"
+        // );
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What artist/band?",
+              name: "bandSelection",
+              default: "Sigrid"
+            }
+          ])
+          .then(function(concertThis) {
+            const seatgeekClientKey = "MTg3NjM0ODJ8MTU3MDMwMjk2Mi41Nw"; // eventually do a dotenv if time
+            // console.log(
+            //   `band selection is working - ${concertThis.bandSelection}`
+            // );
+            const bandSearchURL = `https:api.seatgeek.com/2/events?performers.slug=${concertThis.bandSelection}&client_id=${seatgeekClientKey}`;
+            axios
+              .get(bandSearchURL)
+              .then(function(response) {
+                const JSONdata = JSON.stringify(response.events, null, 2);
+                // console.log(JSONdata);
+                const concertData = [
+                  "Artist: " + JSONdata.title,
+                  "Venue: " +
+                    JSONdata.venue.name +
+                    ", " +
+                    JSONdata.venue.display_location,
+                  "Date of the Show: " +
+                    moment.utc(JSONdata.daytime_local).format("MM-DD-YYYY")
+                ].join("\n");
+                console.log("\n" + concertData + divider);
+              })
+              .catch(function(err, data) {
+                if (err) {
+                  return console.log("Error occurred: " + err);
+                }
+              }); // closes axios search
+          }); // closes the then concert this function for band searches
       }
       if (searchType.selection === "Find a song on Spotify") {
         inquirer
@@ -56,42 +95,96 @@ function cli() {
                 if (err) {
                   return console.log("Error occurred: " + err);
                 }
-                //   console.log(JSON.stringify(data.tracks.items[0], null, 5));
-                console.log(
-                  "Song Name: ",
-                  JSON.stringify(data.tracks.items[0].name, null, 2)
-                );
-                console.log(
-                  "Performed by: ",
-                  JSON.stringify(
-                    data.tracks.items[0].album.artists[0].name,
-                    null,
-                    2
-                  )
-                );
-                console.log(
-                  "From the Album: ",
-                  JSON.stringify(data.tracks.items[0].album.name, null, 2)
-                );
-                console.log(
-                  "Preview Link: ",
-                  JSON.stringify(
-                    data.tracks.items[0].external_urls.spotify,
-                    null,
-                    2
-                  )
-                );
+                // //   console.log(JSON.stringify(data.tracks.items[0], null, 5));
+                // console.log(
+                //   "Song Name: ",
+                //   JSON.stringify(data.tracks.items[0].name, null, 2)
+                // );
+                // console.log(
+                //   "Performed by: ",
+                //   JSON.stringify(
+                //     data.tracks.items[0].album.artists[0].name,
+                //     null,
+                //     2
+                //   )
+                // );
+                // console.log(
+                //   "From the Album: ",
+                //   JSON.stringify(data.tracks.items[0].album.name, null, 2)
+                // );
+                // console.log(
+                //   "Preview Link: ",
+                //   JSON.stringify(
+                //     data.tracks.items[0].external_urls.spotify,
+                //     null,
+                //     2
+                //   )
+                // );
+                const songData = [
+                  "Song: " + JSON.stringify(data.tracks.items[0].name, null, 2),
+                  "Performed by: " +
+                    JSON.stringify(
+                      data.tracks.items[0].album.artists[0].name,
+                      null,
+                      2
+                    ),
+                  "From the album: " +
+                    JSON.stringify(data.tracks.items[0].album.name, null, 2),
+                  "Preview link: " +
+                    JSON.stringify(
+                      data.tracks.items[0].external_urls.spotify,
+                      null,
+                      2
+                    )
+                ].join("\n");
+                // add to the file and print
+                fs.appendFile("log.txt", songData + divider, function(err) {
+                  if (err) throw err;
+                  console.log("\n" + songData + divider);
+                }); // closes append
               }
             ); // closes spotify API search
           }); // closes then for spotify prompt
-      } // closes if for "Find a song on Spotify
-      // } // closes spotifyThisSong function
+      } // closes spotify if
       if (searchType.selection === "Find information on your favorite movie") {
         // function(concertThis) {
-        console.log(
-          "HERE YOU WILL PUT THE PROMPT FOR MOVIES followed by the search"
-        );
-      } // closes movie movie-this if
+        // console.log(
+        //   "HERE YOU WILL PUT THE PROMPT FOR MOVIES followed by the search"
+        // );
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What movie?",
+              name: "movieSelection",
+              default: "Mr. Nobody"
+            }
+          ])
+          .then(function(movieThis) {
+            const movieSearchURL = `http://www.omdbapi.com/?t=${movieThis.movieSelection}&apikey=trilogy`;
+            // console.log(`The movie you chose is ` + movieThis.movieSelection);
+            axios
+              .get(movieSearchURL)
+              .then(function(response) {
+                const JSONmovieData = JSON.stringify(response, null, 2);
+                console.log(JSONmovieData.Title);
+              }) // closes then response
+              .catch(function(err, data) {
+                if (err) {
+                  return console.log("Error occurred: " + err);
+                }
+              });
+          }); // closes then movieThis
+
+        // * Title of the movie.
+        // * Year the movie came out.
+        // * IMDB Rating of the movie.
+        // * Rotten Tomatoes Rating of the movie.
+        // * Country where the movie was produced.
+        // * Language of the movie.
+        // * Plot of the movie.
+        // * Actors in the movie.
+      } // closes movie if
       if (searchType.selection === "Search for something random") {
         // function(concertThis) {
         console.log(
